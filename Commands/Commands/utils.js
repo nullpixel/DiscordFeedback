e375b2129926bf03697b0edfcc93dccacf6d1a73
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2017-01-13 14:44:15 -0500

    Add rudimentary search functionality. Also add sendTyping for the delay it takes to return info. Search still needs changes that Dabbit suggested in the Private testing server for the bot.

--- Commands/Commands/utils.js
+++ Commands/Commands/utils.js
@@ -40,55 +40,101 @@ commands['uv-suggest'] = {
   modOnly: false,
   fn: function (client, message, suffix, UserVoice, uvClient, Config) {
     if (suffix === 'list') {
+      message.channel.sendTyping()
       listSuggestions(Config, uvClient)
-      .then(function (suggestionsList) {
-        suggestionsList.suggestions.forEach(function (suggestions) {
-          message.channel.sendMessage('', false, {
-            title: suggestions.title,
-            url: suggestions.url,
-            timestamp: suggestions.created_at,
-            color: 0x3498db,
-            author: {
-              name: suggestions.creator.name,
-              url: suggestions.creator.url,
-              icon_url: suggestions.creator.avatar_url
-            },
-            fields: [
-              {
-                name: 'Votes',
-                value: suggestions.vote_count
-              },
-              {
-                name: 'Created',
-                value: suggestions.topic.created_at
-              },
-              {
-                name: 'Last updated',
-                value: suggestions.topic.updated_at
-              }
-            ]
-          })
-          .catch(function (e) {
-            console.log(e.response.error)
-            message.channel.sendMessage(['There was an error:' + '\n```\n' + e + '\n```'])
+        .then(function (suggestionsList) {
+          suggestionsList.suggestions.forEach(function (suggestions) {
+            message.channel.sendMessage('', false, {
+                title: suggestions.title,
+                url: suggestions.url,
+                timestamp: suggestions.created_at,
+                color: 0x3498db,
+                author: {
+                  name: suggestions.creator.name,
+                  url: suggestions.creator.url,
+                  icon_url: suggestions.creator.avatar_url
+                },
+                fields: [{
+                    name: 'Votes',
+                    value: suggestions.vote_count
+                  },
+                  {
+                    name: 'Created',
+                    value: suggestions.topic.created_at
+                  },
+                  {
+                    name: 'Last updated',
+                    value: suggestions.topic.updated_at
+                  }
+                ]
+              })
+              .catch(function (e) {
+                console.log(e.response.error)
+                message.channel.sendMessage(['There was an error:' + '\n```\n' + e + '\n```'])
+              })
           })
         })
-      })
-      .catch(function (e) {
-        console.log(e)
-        message.channel.sendMessage(['There was an error:' + '\n```\n' + e + '\n```'])
-      })
+        .catch(function (e) {
+          console.log(e)
+          message.channel.sendMessage(['There was an error:' + '\n```\n' + e + '\n```'])
+        })
+    } else if (suffix === null) {
+      message.channel.sendMessage('You need to specify a command.')
+    } else {
+      message.channel.sendMessage('No such command. :cry:')
+    }
+  }
+}
+
+commands['uv-search'] = {
+  adminOnly: true,
+  modOnly: false,
+  fn: function (client, message, suffix, UserVoice, uvClient, Config) {
+    message.channel.sendTyping()
+    if (suffix === null) {
+      message.channel.sendMessage('You need to specify a search term.')
+    } else {
+      search(Config, uvClient, suffix)
+        .then(function (search) {
+          search.suggestions.forEach(function (suggest) {
+            var searchSuggest = JSON.stringify(suggest)
+            message.channel.sendMessage(['\n```\n' + searchSuggest + '\n```'])
+              .catch(function (e) {
+                console.log(e)
+                message.channel.sendMessage(['There was an error:' + '\n```\n' + e + '\n```'])
+              })
+          })
+        })
+        .catch(function (e) {
+          console.log(e)
+          var er = JSON.stringify(e)
+          message.channel.sendMessage(['There was an error:' + '\n```\n' + er + '\n```'])
+        })
     }
   }
 }
 
 exports.Commands = commands
 
-function listSuggestions (Config, uvClient) {
+function listSuggestions(Config, uvClient) {
   return new Promise((resolve, reject) => {
     uvClient.loginAsOwner()
     var uV = ['forums/' + Config.uservoice.forumId.trim() + '/suggestions.json']
-    uvClient.get(uV.toString())
+    uvClient.get(uV.toString(), {
+        sort: 'newest'
+      })
+      .then(resolve)
+      .catch(reject)
+  })
+}
+
+function search(Config, uvClient, query) {
+  return new Promise((resolve, reject) => {
+    uvClient.loginAsOwner()
+    var uV = ['forums/' + Config.uservoice.forumId.trim() + '/suggestions/search.json']
+    uvClient.get(uV.toString(), {
+        query: query
+      })
       .then(resolve)
       .catch(reject)
   })
